bacb226a513a5120b6bab6cd293e4e6833959bb4
Yi-cell <74481620+Yi-cell@users.noreply.github.com>
2020-12-10 16:43:04 +1000

    add simple knn

--- K-NN.py
+++ K-NN.py
@@ -4,7 +4,51 @@ import numpy as np
 
 test = pd.read_csv('Predict Health Insurance/test.csv')
 train = pd.read_csv('Predict Health Insurance/train.csv')
+'''
 print(test.head())
 print(test.shape)
 print(train.head())
+print('-------')
 print(train.shape)
+print(test.isnull().sum())
+# try two variables (Annual_premium & Region_Code & Vintage)
+numerical_columns = ['Annual_Premium','Region_Code','Vintage']
+print(train[numerical_columns].describe())
+print(len(train[numerical_columns]))
+returnMat = np.zeros((len(train[numerical_columns]),3))
+print(returnMat)
+classLabelVactor = []
+index = 0
+ 
+returnMat[index,:] = train[numerical_columns].iloc[:,0:3]
+
+print(train[numerical_columns].iloc[:,0:3]) 
+print(returnMat)
+'''
+'''
+def knn(train,test,labels,k):
+    # number of rows
+    row_size = train.shape[0]
+    # calculate the difference 
+    diff = np.tile(test,(row_size,1)) - train
+    # sum of square 
+    sqrdiff = diff ** 2
+    sqrdiffsum = sqrdiff.sum(axis = 1)
+    # calculate the distance
+    distances = sqrdiffsum ** 0.5
+    # order by distance
+    sortdistances = distances.argsort()
+
+    count = {}
+    for i in range(k):
+'''
+def file_matrix(filename):
+    rows_size = len(train)
+    return_mat = np.zeros((rows_size,3))
+    label_vector = []
+    index = 0
+    
+    for line in train:
+        print (line)
+
+ from sklearn.preprocessing import StandardScaler
---
+++ simpleknn.py
@@ -0,0 +1,49 @@
+import numpy as np
+import operator
+def create_dataset():
+    # four group features
+    group = np.array([[1,101],[5,89],[108,5],[115,8]])
+    # four group labels
+    labels = ['Romantic','Romantic','Action','Action']
+    return group,labels
+
+def classifier(inx,dataset,labels,k):
+    '''
+    Parameters:
+    inx: test dataset
+    dataset: train dataset
+    labels: classify labels 
+    k: choose k points which has smartest distance
+    Return:
+    sorted_class_count[0][0]: classification results
+    '''
+    #dataset rowsize
+    datasetsize = dataset.shape[0]
+    #creat matrix 
+    diffmat = np.tile(inx,(datasetsize,1))-dataset 
+    #sum of square
+    sq_diffmat = diffmat**2
+    sqdistance = sq_diffmat.sum(axis=1)
+    #calculate distance
+    distances = sqdistance**0.5
+    #order by distance
+    sorted_distance_indices = distances.argsort()
+    #
+    class_count={}
+    for i in range(k):
+        vote_label = labels[sorted_distance_indices[i]]
+        class_count[vote_label] = class_count.get(vote_label,0)+1
+        
+    sorted_class_count = sorted(class_count.items(),key=operator.itemgetter(1),reverse=True)
+    return sorted_class_count[0][0]
+if __name__ == '__main__':
+    group,labels = create_dataset()
+
+    test = [101,20]
+    test_class = classifier(test,group,labels,3)
+    print(test_class)
+
+
+
+
+
