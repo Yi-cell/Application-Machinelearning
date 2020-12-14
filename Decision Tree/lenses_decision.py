fcf7aef152e9f47e754344e1a35713b474984817
Yi-cell <74481620+Yi-cell@users.noreply.github.com>
2020-12-14 17:33:29 +1000

    Second update

--- Decision Tree/lenses_decision.py
+++ Decision Tree/lenses_decision.py
@@ -1,1 +1,10 @@
 from sklearn import tree
+import pandas as pd 
+
+if __name__ =='__main__':
+    fr = open('lenses.txt')
+    lenses = [instance.strip().split('\t') for instance in fr.readlines()]
+    print(lenses)
+    lenses_labels = ['age','prescript','astigmatic','tearRate']
+    clf = tree.DecisionTreeClassifier()
+    lenses = clf.fit(lenses,lenses_labels)
