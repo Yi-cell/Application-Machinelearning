6c35379f7fc776a8054950162c552abd5dc40628
Yi-cell <74481620+Yi-cell@users.noreply.github.com>
2020-12-14 18:16:58 +1000

    update

--- Decision Tree/lenses_decision.py
+++ Decision Tree/lenses_decision.py
@@ -4,7 +4,20 @@ import pandas as pd
 if __name__ =='__main__':
     fr = open('lenses.txt')
     lenses = [instance.strip().split('\t') for instance in fr.readlines()]
-    print(lenses)
+    lenses_target = []
+    for each in lenses:
+        lenses_target.append(each[-1])
+
     lenses_labels = ['age','prescript','astigmatic','tearRate']
-    clf = tree.DecisionTreeClassifier()
-    lenses = clf.fit(lenses,lenses_labels)
+    lenses_list=[] #save the lenses data temporarily
+    lenses_dict={} #save the lenses data dictionary
+
+    for each_label in lenses_labels:
+        for each in lenses:
+            lenses_list.append(each[lenses_labels.index(each_label)])
+        lenses_dict[each_label] = lenses_list
+        lenses_list=[]
+    print(lenses_dict)
+    lenses_pd = pd.DataFrame(lenses_dict)
+    print(lenses_pd)
+    
